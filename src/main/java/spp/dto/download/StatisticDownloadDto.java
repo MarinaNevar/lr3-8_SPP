ea6ad133bbe34a1b35956dc852ac779af7d347ae
TemaFilimonov <filart18@mail.ru>
2017-05-07 15:24:17 +0300

    fix

--- src/main/java/spp/dto/download/StatisticDownloadDto.java
+++ src/main/java/spp/dto/download/StatisticDownloadDto.java
@@ -1,17 +1,8 @@
 package spp.dto.download;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 /**
  * Created by admin on 07.05.2017.
  */
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class StatisticDownloadDto {
     //TODO generate statistic from front-end data(Waiting S.Karaschuk feedback)
 }
