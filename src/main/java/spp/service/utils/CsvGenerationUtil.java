068921a33a4693abacb595194cc95e6b316fc358
Dima Krivolap <dima_juv@mail.ru>
2017-05-08 17:01:28 +0300

    add GereratorResumeInCSV add GereratorUserInCSV add GereratorProjectInCSV

--- src/main/java/spp/service/utils/CsvGenerationUtil.java
+++ src/main/java/spp/service/utils/CsvGenerationUtil.java
@@ -2,7 +2,8 @@ package spp.service.utils;
 
 
 import au.com.bytecode.opencsv.CSVWriter;
-import spp.dto.download.VacancyDownloadDto;
+import spp.dto.download.*;
+;
 import spp.entity.Vacancy;
 
 import java.io.ByteArrayOutputStream;
@@ -13,7 +14,7 @@ import java.util.*;
 
 public class CsvGenerationUtil {
 
-    public static ByteArrayOutputStream generateVacanciesInCSV(VacancyDownloadDto vacancy) throws IOException {
+    public static ByteArrayOutputStream generateVacanciesInCSV(VacancyDownloadDto object) throws IOException {
         String[] fileHeader = null;
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
@@ -22,17 +23,76 @@ public class CsvGenerationUtil {
         writer.writeNext(fileHeader);
         List<String[]> vacanciesInString = new ArrayList<>();
         vacanciesInString.add(new String[]{
-                vacancy.getOwnerName(),
-                vacancy.getTitle(),
-                vacancy.getAbout(),
-                vacancy.getProjectTitle(),
-                vacancy.getCreationDate().toString(),
+                object.getOwnerName(),
+                object.getTitle(),
+                object.getAbout(),
+                object.getProjectTitle(),
+                object.getCreationDate().toString(),
                 });
         writer.writeAll(vacanciesInString);
         writer.close();
         return stream;
     }
 
+    public static ByteArrayOutputStream generateResumeInCSV(ResumeDownloadDto object) throws IOException {
+        String[] fileHeader = null;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        OutputStreamWriter os = new OutputStreamWriter(stream, Charset.forName("cp1251"));
+        CSVWriter writer = new CSVWriter(os, ',');
+        writer.writeNext(fileHeader);
+        List<String[]> vacanciesInString = new ArrayList<>();
+        vacanciesInString.add(new String[]{
+                object.getTitle(),
+                object.getCreationDate().toString(),
+                object.getOwnerName(),
+                object.getAbout(),
+                object.getExpJson(),
+                object.getSkillsJson()
+        });
+        writer.writeAll(vacanciesInString);
+        writer.close();
+        return stream;
+    }
+
+    public static ByteArrayOutputStream generateProjectInCSV(ProjectDownloadDto object) throws IOException {
+        String[] fileHeader = null;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        OutputStreamWriter os = new OutputStreamWriter(stream, Charset.forName("cp1251"));
+        CSVWriter writer = new CSVWriter(os, ',');
+        writer.writeNext(fileHeader);
+        List<String[]> vacanciesInString = new ArrayList<>();
+        vacanciesInString.add(new String[]{
+                object.getTitle(),
+                object.getCreationDate().toString(),
+                object.getOwnerName(),
+                object.getAbout(),
+                object.getVacancies().toString()
+
+        });
+        writer.writeAll(vacanciesInString);
+        writer.close();
+        return stream;
+    }
+
+    public static ByteArrayOutputStream generateUserInCSV(UserDownloadDto object) throws IOException {
+        String[] fileHeader = null;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        OutputStreamWriter os = new OutputStreamWriter(stream, Charset.forName("cp1251"));
+        CSVWriter writer = new CSVWriter(os, ',');
+        writer.writeNext(fileHeader);
+        List<String[]> vacanciesInString = new ArrayList<>();
+        vacanciesInString.add(new String[]{
+                object.getUserName(),
+                object.getResumes().toString()
+
+        });
+        writer.writeAll(vacanciesInString);
+        writer.close();
+        return stream;
+    }
 
 
 }
