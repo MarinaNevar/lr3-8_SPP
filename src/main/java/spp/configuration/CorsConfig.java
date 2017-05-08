f386466fe32d01f994bb0394b416307cd55832b6
TemaFilimonov <filart18@mail.ru>
2017-05-09 01:26:42 +0300

    cors config

---
+++ src/main/java/spp/configuration/CorsConfig.java
@@ -0,0 +1,26 @@
+package spp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by admin on 09.05.2017.
+ */
+@Configuration
+public class CorsConfig {
+
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurerAdapter() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/.*");
+                }
+            };
+
+    }
+}
