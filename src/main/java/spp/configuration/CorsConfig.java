db916439c61eecfc470a95ba5480cfcc3986d8a7
TemaFilimonov <filart18@mail.ru>
2017-05-09 02:18:51 +0300

    cors fix

--- src/main/java/spp/configuration/CorsConfig.java
+++ src/main/java/spp/configuration/CorsConfig.java
@@ -20,7 +20,14 @@ public class CorsConfig {
             return new WebMvcConfigurerAdapter() {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**");
+                    registry.addMapping("*")
+                            .allowedMethods(
+                                    "PUT",
+                                    "DELETE",
+                                    "GET",
+                                    "POST"
+                            );
+
                 }
             };
 
